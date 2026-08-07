91fb1bf4778bc92a2d9129612c66a5ea7db24f90
Pisedhysara Vong <pisethysaravong@gmail.com>
2026-08-07 16:07:42 +0700

    added jwt secret keys, google client id, and test the build

--- src/app/resources/r1-account/module.ts
+++ src/app/resources/r1-account/module.ts
@@ -5,12 +5,13 @@ import { ConfigModule } from '@nestjs/config';
 
 // ===========================================================================>> Custom Library
 import { AuthController } from './controller';
+import { JwtService } from '@nestjs/jwt';
 import { AuthService } from './service';
 
 @Module({
   imports: [ConfigModule],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtService],
   exports: [AuthService],
 })
 export class AccountModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import 'dotenv/config'; // must be first line
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
-// import cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.use(cookieParser());
+  app.use(cookieParser());
   app.enableCors({
     origin: process.env.WEB_URL || 'http://localhost:4000', // must be exact origin, not '*'
     credentials: true, // this line is critical
